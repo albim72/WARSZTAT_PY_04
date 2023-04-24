57f0218d58b1dfdc57cbda8081e61b379f401878
albim72 <marcin.albiniak72@gmail.com>
2023-04-24 15:51:49 +0200

    Create bookstore.py

---
+++ DZIEN_1/bookstore.py
@@ -0,0 +1,39 @@
+class Book:
+    #deklaracja stanu(dane) -> konstruktor klasy
+    # def __new__(cls, *args, **kwargs):
+    #     return object.__new__(cls, *args, **kwargs)
+    def __init__(self,id,tytul,autor,cena=10):
+        self.idbook = id
+        self.tytul = tytul
+        self.autor = autor
+        self.cena = cena
+        self.oprawa = "miękka"
+        self.create_book()
+
+    #definicja zachowania -> funkcje klasy -> metody
+    def create_book(self):
+        print("utworzono nową książkę!")
+
+    def printbook(self):
+        return f"książka {self.tytul}, autor: {self.autor}, cena: {self.cena} zł, oprawa: {self.oprawa}"
+
+    def rabat(self,procent):
+        return round(self.cena*(procent/100),2)
+
+    def setcena(self,nowacena):
+        self.cena = nowacena
+
+bk = Book(3,"Wiedźmin","Andrzej Sapkowski")
+print(bk)
+print(bk.printbook())
+print(f"rabat od ceny: {bk.rabat(12)} zł")
+
+bk.cena = 44
+print("********************************")
+print(bk.printbook())
+print(f"rabat od ceny: {bk.rabat(12)} zł")
+
+bk.setcena(41)
+print("********************************")
+print(bk.printbook())
+print(f"rabat od ceny: {bk.rabat(12)} zł")
