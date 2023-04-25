b837bdc27e01e12117917960f10614049cc13b93
albim72 <marcin.albiniak72@gmail.com>
2023-04-25 09:28:03 +0200

    Create main.py

---
+++ DZIEN_2/moje_moduly/main.py
@@ -0,0 +1,11 @@
+#import dane
+#import dane as dn
+from dane import nr_filii as filia
+from dane import book as bk
+
+print("wywołanie bespośrednie")
+print("*"*36)
+print(filia)
+print(bk)
+
+print("*"*36)
