8aac7a3fb4fb6ce79a8b45eeef54e283d380c323
albim72 <marcin.albiniak72@gmail.com>
2023-04-25 11:49:33 +0200

    Create klient.py

---
+++ DZIEN_2/fitness_klub/klient.py
@@ -0,0 +1,20 @@
+from trener import Trener
+
+class Klient(Trener):
+    def __init__(self, imie, wiek, waga, wzrost,
+                 id_klienta,rodzaj_karnetu,
+                 nr_kb, nazwa_kb, miasto, dyscyplina,
+                 lata_upr, wynik,
+                 nr_licencji="", dosw_lata="", expert=""):
+        super().__init__(imie, wiek, waga, wzrost, nr_licencji, dosw_lata, expert, nr_kb, nazwa_kb, miasto, dyscyplina,
+                         lata_upr, wynik)
+        self.id_klienta = id_klienta
+        self.rodzaj_karnetu = rodzaj_karnetu
+        print(self.przydzial_nr())
+        if self.nr_licencji != "":
+            self.trener = True
+        else:
+            self.trener = False
+
+    def przydzial_nr(self):
+        return f"przydzielono numer klienta: {self.id_klienta}."
