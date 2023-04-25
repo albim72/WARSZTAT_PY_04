7996fd42b441254dc30986062cda54f6f9a92615
albim72 <marcin.albiniak72@gmail.com>
2023-04-25 11:32:49 +0200

    Update mian.py

--- DZIEN_2/fitness_klub/mian.py
+++ DZIEN_2/fitness_klub/mian.py
@@ -1,4 +1,5 @@
 from osoba import Osoba
+from trener import Trener
 
 print("Obiekty klasy Osoba")
 print("*"*36)
@@ -6,3 +7,13 @@ os1 = Osoba("Feliks",43,88,178)
 print(os1)
 os1.print_osoba()
 print(f"czy osoba jest Trenerem? -> {os1.czytrener()}")
+
+print("Obiekty klasy Trener")
+print("*"*36)
+
+tr1 = Trener("Olga",27,52,173,5564,7,True,5,"Samson","Kraków","Trójbój",4,"340kg")
+tr1.print_osoba()
+tr1.print_trener()
+tr1.infoklub()
+tr1.infosport()
+
