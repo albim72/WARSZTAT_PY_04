ca68d94e4fb1a1d062814ebd4ec93625427e6fcc
albim72 <marcin.albiniak72@gmail.com>
2023-04-25 14:24:55 +0200

    Create silnia.py

---
+++ DZIEN_2/silnia.py
@@ -0,0 +1,32 @@
+#funkcja silnia n!=1*2*...*n
+#double -> 1.8E+308, min -2.4E+234
+#n! ???? 171!
+
+import sys
+
+sys.set_int_max_str_digits(0x1000000)
+sys.setrecursionlimit(0x1000000)
+
+def silnia(n):
+    if n<0:
+        raise ValueError("Silnia nie jest zdefiniowana dla liczb ujemnych!")
+    wynik =1
+    for i in range(1,n+1):
+        wynik *= i
+    return wynik
+
+def silnia_rek(n):
+    if n<0:
+        raise ValueError("Silnia nie jest zdefiniowana dla liczb ujemnych!")
+    if n==0:
+        return 1
+    else:
+        return n*silnia_rek(n-1)
+
+try:
+    n = int(input("podaj wartość argumentu n funkcji silnia: "))
+    print(f"silnia dla wartości n = {n} wynosi: {silnia(n)}")
+    print(f"silnia rekurencyjna dla wartości n = {n} wynosi: {silnia_rek(n)}")
+except ValueError as ve:
+    print(ve)
+
