07edd937086160ed4fea29bd28abb7db70a54ae2
albim72 <marcin.albiniak72@gmail.com>
2023-04-25 11:00:56 +0200

    Update sport.py

--- DZIEN_2/fitness_klub/sport.py
+++ DZIEN_2/fitness_klub/sport.py
@@ -4,10 +4,14 @@ class Sport:
         self.lata_upr = lata_upr
         self.wynik = wynik
         print(self.sport_przypisanie())
-        
+
     def sport_przypisanie(self):
         return f"dysycyplina: {self.dyscyplina}"
-        
+
     def infosport(self):
         print(f"{self.dyscyplina}, czs uprawiania [lata]: {self.lata_upr}, "
               f"życiówka: {self.wynik}.")
+
+        
+class Ekstra:
+    pass
