7e45f8b7b3f225ec5eda68253fa70474b94b4ff1
albim72 <marcin.albiniak72@gmail.com>
2023-04-25 10:24:57 +0200

    Update osoba.py

--- DZIEN_2/fitness_klub/osoba.py
+++ DZIEN_2/fitness_klub/osoba.py
@@ -7,10 +7,23 @@ class Osoba:
         self.kolor_oczu = "brązowe"
         self.trener = False
         print("utworzono nowy obiekt klasy osoba...")
-        
+
+    #funkcja konstrukcyjna (specjalna) która tworzy reprezentację tekstową obiektu i zasłania
+    #podstawową definicję obiektu
+    def __repr__(self):
+        return f"Nowa osoba -> kolor oczu: {self.kolor_oczu}"
+
     def print_osoba(self):
         print(f"osoba -> {self.imie}, wiek: {self.wiek} lat, waga: {self.waga} kg, "
-              f"wzrost: {self.wzrost} cm.")
-        
+              f"wzrost: {self.wzrost} cm")
+
     def czytrener(self):
         return self.trener
+
+
+
+
+
+
+
+
