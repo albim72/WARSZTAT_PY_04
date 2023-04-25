a50b370c332c4fcd735d27f38c76ad6645abe2d2
albim72 <marcin.albiniak72@gmail.com>
2023-04-25 09:38:39 +0200

    Update main.py

--- DZIEN_2/moje_moduly/main.py
+++ DZIEN_2/moje_moduly/main.py
@@ -2,6 +2,7 @@
 #import dane as dn
 from dane import nr_filii as filia
 from dane import book as bk
+from funkcje.bfunkcje import czytaj_liste,czytaj_slownik
 
 print("wywołanie bespośrednie")
 print("*"*36)
@@ -9,3 +10,10 @@ print(filia)
 print(bk)
 
 print("*"*36)
+
+print("wywołanie przez funkcję...")
+print("*"*36)
+
+czytaj_liste(filia)
+print("_"*36)
+czytaj_slownik(bk)
