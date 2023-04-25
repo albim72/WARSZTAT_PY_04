9eebf5b0ffc6504b00716abf25b144181e7a8673
albim72 <marcin.albiniak72@gmail.com>
2023-04-25 11:18:41 +0200

    Create trener.py

---
+++ DZIEN_2/fitness_klub/trener.py
@@ -0,0 +1,20 @@
+from osoba import Osoba
+from klub import Klub
+from sport import Sport,Ekstra
+
+class Trener(Osoba,Klub,Sport,Ekstra):
+    def __init__(self, imie, wiek, waga, wzrost,
+                 nr_licencji,dosw_lata,expert,
+                 nr_kb,nazwa_kb,miasto,
+                 dyscyplina,lata_upr,wynik):
+        Osoba.__init__(self,imie, wiek, waga, wzrost)
+        Klub.__init__(self,nr_kb,nazwa_kb,miasto)
+        Sport.__init__(self,dyscyplina,lata_upr,wynik)
+        self.nr_licencji = nr_licencji
+        self.dosw_lata = dosw_lata
+        self.expert = expert
+        self.trener = True
+        
+    def print_trener(self):
+        print(f"Trener -> licencja {self.nr_licencji}, lata doświadczenia: {self.dosw_lata}, "
+              f"ekspert? {self.expert}")
